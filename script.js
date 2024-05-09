88bd32e2aadc5b8d7b86c477fd2d337ac3c9af2b
Sujalkhanal0 <129717349+Sujalkhanal0@users.noreply.github.com>
2024-05-09 08:54:07 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-const topbtn = document.querySelector('span');
-window.addEventListener("scroll",()=>{
-   if (pageYOffset > 100){
-   	topbtn.classList.add('active');
-   }else{
-   	topbtn.classList.remove('active')
-   		
-   }
-})
 
-const removebtn = document.querySelector('.down');
-window.addEventListener('scroll',()=>{
-	if (pageYOffset>300) {
-		removebtn.classList.add('remove');
-	} else {
-		removebtn.classList.remove('remove');
-	}
-});
